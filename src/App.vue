2289a561ad215bbaa5dd130a2798e4ad8d01a085
Sefa <60031458+fesa6654@users.noreply.github.com>
2020-09-10 00:03:03 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <button @click="pdfler()">sefa</button>
+  </div>
+</template>
+
+<script>
+import { jsPDF } from "jspdf";
+
+export default {
+  mounted() {
+    //this.pdfler();
+  },
+  methods: {
+    pdfler() {
+      const doc = new jsPDF();
+
+      doc.text("Hello world!", 10, 10);
+      doc.save("a4.pdf");
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <button @click="pdfac()">sefa</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    pdfac() {
+      this.$router.push("/about");
+    },
+  },
+};
+</script>
